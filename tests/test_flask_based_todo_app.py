5f74369b68a2780ae82b462504ae7a1dce1f9cb2
djfedos <djfedos@djfedos.com>
2022-08-07 17:46:49 +0300

    Add tests

--- tests/test_flask_based_todo_app.py
+++ tests/test_flask_based_todo_app.py
@@ -1,9 +1,10 @@
 import requests
 from my_app import app
 
+
 def test_home_page():
     app.run()
-    response = requests.get('/login')
+    response = requests.get('0.0.0.0:5000/login')
     assert response.status_code == 200
     assert b"Stay Organized!" in response.data
 
