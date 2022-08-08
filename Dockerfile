5f3c363ac35f6c924b456417c8086ec1390aeaf7
djfedos <54898633+djfedos@users.noreply.github.com>
2022-08-08 16:18:19 +0300

    Add tests (#3)
    * Add tests, taskfiles and GitHub actions

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.9
+
+WORKDIR /usr/src/app
+
+COPY flask_app flask_app
+RUN ["/bin/bash", "-c", "python -m venv venv"]
+RUN ["/bin/bash", "-c", "source venv/bin/activate && pip install flask flask_sqlalchemy flask_login flask_wtf"]
+
+
+CMD ["/bin/bash", "-c", "source venv/bin/activate && python flask_app/run.py"]
--- tests/test_flask_based_todo_app.py
+++ tests/test_flask_based_todo_app.py
@@ -1,10 +1,7 @@
 import requests
-from my_app import app
+
 
 def test_home_page():
-    app.run()
-    response = requests.get('/login')
+    response = requests.get('http://127.0.0.1:5000/login')
     assert response.status_code == 200
-    assert b"Stay Organized!" in response.data
-
-
+    assert "Stay Organized!" in response.text
