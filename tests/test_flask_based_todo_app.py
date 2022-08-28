d322692f56855d873d9f0b0b94de4cc046119634
djfedos <djfedos@djfedos.com>
2022-08-28 08:34:44 +0300

    rm test folder (not in use any more), add task descriptions

--- tests/test_flask_based_todo_app.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-def test_home_page():
-    response = requests.get('http://127.0.0.1:5000/login')
-    assert response.status_code == 200
-    assert "Stay Organized!" in response.text
